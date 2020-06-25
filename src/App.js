2d0eb786825f93fea494dadec8bb79d1e9afa69f
mahmud <you@example.com>
2020-06-25 12:34:51 +0600

    directory and pages created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './default.scss'
 import Header from './components/Header'
+import HomePage from './components/pages/Homepage'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h2>Hello World</h2>
+      <HomePage />
         
     </div>
   );
---
+++ src/components/pages/Homepage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Homepage() {
+    return (
+        <section>
+            <h2>
+                Homepage
+            </h2>
+        </section>
+    )
+}
+
+export default Homepage
