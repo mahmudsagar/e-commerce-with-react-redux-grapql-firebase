46d67a6ed97e387ea4530f5e66cd18cca3825ee9
mahmud <you@example.com>
2020-06-25 11:50:50 +0600

    header compnent created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import './default.scss'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <h2>Hello World</h2>
         
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style.scss'
+
+
+import Logo from '../../assets/logo.png'
+
+function Header() {
+    return (
+      <header className="header">
+        <div className="wrap">
+          <div className="logo">
+            <img src={Logo} alt="Logo" />
+          </div>
+        </div>
+      </header>
+    );
+}
+
+export default Header
